edcce1b62299df686ff4aecfb6f7a717db19aceb
Nishant Shukla <114081634+NishantShukla10@users.noreply.github.com>
2024-02-21 13:14:10 +0530

    Add files via upload

---
+++ src/components/CartIcon.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+const CartIcon = () => {
+  return (
+    <div>
+      <Link href="/cart" className='flex items-center gap-1' >
+        <div className='relative w-8 h-8 md:w-5 md:h-5'>
+            <Image src="/cart.png" alt="" fill />
+        </div>
+        <span>Cart (3)</span>
+      </Link>
+    </div>
+  )
+}
+
+export default CartIcon
